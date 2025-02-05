5628eedc3a7decad02d6d32cb643f332099de6dc
Kaj Bromose <kbr@ucl.dk>
2025-02-05 12:03:06 +0100

    Add Dapr State Store to .NET Aspire resources Add Dapr SubSub to .NET Aspire resources Add Dapr sidecar to .NET Aspire resources

--- start-here/DaprWorkshop.AppHost/Program.cs
+++ start-here/DaprWorkshop.AppHost/Program.cs
@@ -1,11 +1,41 @@
+using Aspire.Hosting.Dapr;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.PizzaStorefront>("pizzastorefront");
+var statestore = builder.AddDaprStateStore("pizzastatestore");
+var pubsubComponent = builder.AddDaprPubSub("pizzapubsub");
 
-builder.AddProject<Projects.PizzaKitchen>("pizzakitchen");
+builder.AddProject<Projects.PizzaOrder>("pizzaorderservice")
+    .WithDaprSidecar(new DaprSidecarOptions
+    {
+        AppId = "pizza-order",
+        DaprHttpPort = 3501
+    })
+    .WithReference(statestore)
+    .WithReference(pubsubComponent);
 
-builder.AddProject<Projects.PizzaDelivery>("pizzadelivery");
+builder.AddProject<Projects.PizzaKitchen>("pizzakitchenservice")
+    .WithDaprSidecar(new DaprSidecarOptions
+    {
+        AppId = "pizza-kitchen",
+        DaprHttpPort = 3503
+    })
+    .WithReference(pubsubComponent);
 
-builder.AddProject<Projects.PizzaOrder>("pizzaorder");
+builder.AddProject<Projects.PizzaStorefront>("pizzastorefrontservice")
+    .WithDaprSidecar(new DaprSidecarOptions
+    {
+        AppId = "pizza-storefront",
+        DaprHttpPort = 3502
+    })
+    .WithReference(pubsubComponent);
+
+builder.AddProject<Projects.PizzaDelivery>("pizzadeliveryservice")
+    .WithDaprSidecar(new DaprSidecarOptions
+    {
+        AppId = "pizza-delivery",
+        DaprHttpPort = 3504
+    })
+    .WithReference(pubsubComponent);
 
 builder.Build().Run();
