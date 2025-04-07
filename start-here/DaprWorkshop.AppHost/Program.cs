19203865a81ccb02eabfdbe2b42cbc57f25bc803
Kaj Bromose <kbr@ucl.dk>
2025-04-07 10:18:43 +0200

    Update to Dapr Runtime version 1.15.4

--- start-here/DaprWorkshop.AppHost/Program.cs
+++ start-here/DaprWorkshop.AppHost/Program.cs
@@ -1,50 +1,52 @@
-using Aspire.Hosting.Dapr;
+using CommunityToolkit.Aspire.Hosting.Dapr;
 using Projects;
+using System.Collections.Immutable;
 
 var builder = DistributedApplication.CreateBuilder(args);
-
-var statestore = builder.AddDaprStateStore("pizzastatestore");
-var pubsubComponent = builder.AddDaprPubSub("pizzapubsub");
 
 builder.AddProject<PizzaOrder>("pizzaorderservice")
     .WithDaprSidecar(new DaprSidecarOptions
     {
         AppId = "pizza-order",
-        DaprHttpPort = 3501
-    }).WithReference(statestore)
-    .WithReference(pubsubComponent);
+        DaprHttpPort = 3501,
+        ResourcesPaths = ImmutableHashSet.Create("../resources")
+    });
 
 
 builder.AddProject<PizzaKitchen>("pizzakitchenservice")
     .WithDaprSidecar(new DaprSidecarOptions
     {
         AppId = "pizza-kitchen",
-        DaprHttpPort = 3503
-    }).WithReference(pubsubComponent);
+        DaprHttpPort = 3503,
+        ResourcesPaths = ImmutableHashSet.Create("../resources")
+    });
 
 
 builder.AddProject<PizzaStorefront>("pizzastorefrontservice")
     .WithDaprSidecar(new DaprSidecarOptions
     {
         AppId = "pizza-storefront",
-        DaprHttpPort = 3502
-    }).WithReference(pubsubComponent);
+        DaprHttpPort = 3502,
+        ResourcesPaths = ImmutableHashSet.Create("../resources")
+    });
 
 
 builder.AddProject<PizzaDelivery>("pizzadeliveryservice")
     .WithDaprSidecar(new DaprSidecarOptions
     {
         AppId = "pizza-delivery",
-        DaprHttpPort = 3504
-    }).WithReference(pubsubComponent);
+        DaprHttpPort = 3504,
+        ResourcesPaths = ImmutableHashSet.Create("../resources")
+    });
 
 
 builder.AddProject<PizzaWorkflow>("pizzaworkflowservice")
     .WithDaprSidecar(new DaprSidecarOptions
     {
         AppId = "pizza-workflow",
-        DaprHttpPort = 3505
-    }).WithReference(statestore).WithReference(pubsubComponent);
+        DaprHttpPort = 3505,
+        ResourcesPaths = ImmutableHashSet.Create("../resources")
+    });
 
 
 builder.Build().Run();
