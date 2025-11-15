9497efb07f3472ca02038ec0e2001283149871b9
Kaj Bromose <kbr@ucl.dk>
2025-11-15 14:00:47 +0100

    Dapr resources added Version bump: Aspire 13 Aspire.Hosting.Dapr 13.0.0-beta.439

--- start-here/DaprWorkshop.AppHost/Program.cs
+++ start-here/DaprWorkshop.AppHost/Program.cs
@@ -1,41 +1,41 @@
 using CommunityToolkit.Aspire.Hosting.Dapr;
+using System.Collections.Immutable;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var statestore = builder.AddDaprStateStore("pizzastatestore");
-var pubsubComponent = builder.AddDaprPubSub("pizzapubsub");
+//var statestore = builder.AddDaprStateStore("pizzastatestore");
+//var pubsubComponent = builder.AddDaprPubSub("pizzapubsub");
 
 builder.AddProject<Projects.PizzaOrder>("pizzaorderservice")
     .WithDaprSidecar(new DaprSidecarOptions
     {
         AppId = "pizzaorder",
-        DaprHttpPort = 3501
-    })
-    .WithReference(statestore)
-    .WithReference(pubsubComponent);
+        DaprHttpPort = 3501,
+        ResourcesPaths = ImmutableHashSet.Create("../resources")
+    });
 
 builder.AddProject<Projects.PizzaKitchen>("pizzakitchenservice")
     .WithDaprSidecar(new DaprSidecarOptions
     {
         AppId = "pizza-kitchen",
-        DaprHttpPort = 3503
-    })
-    .WithReference(pubsubComponent);
+        DaprHttpPort = 3503,
+        ResourcesPaths = ImmutableHashSet.Create("../resources")
+    });
 
 builder.AddProject<Projects.PizzaStorefront>("pizzastorefrontservice")
     .WithDaprSidecar(new DaprSidecarOptions
     {
         AppId = "pizza-storefront",
-        DaprHttpPort = 3502
-    })
-    .WithReference(pubsubComponent);
+        DaprHttpPort = 3502,
+        ResourcesPaths = ImmutableHashSet.Create("../resources")
+    });
 
 builder.AddProject<Projects.PizzaDelivery>("pizzadeliveryservice")
     .WithDaprSidecar(new DaprSidecarOptions
     {
         AppId = "pizza-delivery",
-        DaprHttpPort = 3504
-    })
-    .WithReference(pubsubComponent);
+        DaprHttpPort = 3504,
+        ResourcesPaths = ImmutableHashSet.Create("../resources")
+    });
 
 builder.Build().Run();
